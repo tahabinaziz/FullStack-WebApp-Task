f1dd58b17d30e1d04790c2cca1f093afd3def911
tahabinaziz <taha-bin.aziz@informatik.hs-fulda.de>
2022-04-19 05:03:03 +0200

    addtocartquantity

--- Frontend/src/app/components/product-cart-dialog/product-cart-dialog.component.ts
+++ Frontend/src/app/components/product-cart-dialog/product-cart-dialog.component.ts
@@ -4,11 +4,11 @@ import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-product-cart-dialog',
   templateUrl: './product-cart-dialog.component.html',
-  styleUrls: ['./product-cart-dialog.component.css']
+  styleUrls: ['./product-cart-dialog.component.css'],
 })
 export class ProductCartDialogComponent implements OnInit {
-  selectedProducts:any = [];
-  constructor(private dataService: DataService ) { }
+  selectedProducts: any = [];
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.castSelectedProducts.subscribe((selectedProducts) => {
@@ -16,21 +16,41 @@ export class ProductCartDialogComponent implements OnInit {
     });
   }
 
-  removeAll(){
-    this.dataService.addSelectedProduct([])
+  removeAll() {
+    this.dataService.addSelectedProduct([]);
   }
 
-  removeItem(id: Number){
-    this.selectedProducts = this.selectedProducts.filter(function(item: any) {
-      return item.id !== id
-  })
-  this.dataService.addSelectedProduct(this.selectedProducts)
-   console.log(this.selectedProducts.id)  
+  removeItem(id: Number) {
+    this.selectedProducts = this.selectedProducts.filter(function (item: any) {
+      return item.id !== id;
+    });
+    this.dataService.addSelectedProduct(this.selectedProducts);
   }
-  getTotalAmount(){
-    const sumValues = this.selectedProducts.reduce((accumulator: any, object: { price: any; }) => {
-      return accumulator + object.price;
-    }, 0);
+  getTotalAmount() {
+    const sumValues = this.selectedProducts.reduce(
+      (accumulator: any, object: { price: any }) => {
+        return accumulator + object.price;
+      },
+      0
+    );
     return sumValues;
+  }
+  addCounter(data: any) {
+    let objIndex = this.selectedProducts.findIndex(
+      (obj: { id: number }) => obj.id === data.id
+    );
+    this.selectedProducts[objIndex].quantity++;
+    this.selectedProducts[objIndex].price = this.selectedProducts[objIndex].price + this.selectedProducts[objIndex].quantity;
+    this.dataService.addSelectedProduct(this.selectedProducts);
+  }
+  removeCounter(data: any) {
+    let objIndex = this.selectedProducts.findIndex(
+      (obj: { id: number }) => obj.id === data.id
+    );
+    if(this.selectedProducts[objIndex].quantity > 0){
+    this.selectedProducts[objIndex].quantity--;
+    // this.selectedProducts[objIndex].price = this.selectedProducts[objIndex].price * this.selectedProducts[objIndex].quantity;
+    this.dataService.addSelectedProduct(this.selectedProducts);
+    }
   }
 }
